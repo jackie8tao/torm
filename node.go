87324421eaea5fb3fb7e39ff45fb627bc89d6f88
jackietao <jackietao@tencent.com>
2020-08-05 07:08:52 +0800

    simplest select statement

--- node.go
+++ node.go
@@ -1,9 +1,13 @@
 package torm
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+)
 
 type NodeType int
 
+// 节点类型标志
 const (
 	NTSelect NodeType = iota + 1
 	NTFrom
@@ -12,9 +16,71 @@ const (
 
 // Node sql语法部分节点
 type Node interface {
-	// Next 返回下一个节点，如果为链表结束，则返回错误
-	Next() (Node, error)
-
 	// Walk 遍历节点，生成sql语句
-	Walk(strings.Builder) error
+	Walk(*strings.Builder) error
+}
+
+// BaseNode 基础节点
+type BaseNode struct {
+	// Type 节点类型
+	Type NodeType
+
+	// Next 下一个节点
+	Next Node
+}
+
+// SelectNode select节点
+type SelectNode struct {
+	BaseNode
+	columns []string
+}
+
+func (s *SelectNode) Walk(sql *strings.Builder) (err error) {
+	_, err = sql.WriteString("SELECT ")
+	if err != nil {
+		return
+	}
+
+	if len(s.columns) <= 0 {
+		sql.WriteString("* ")
+	} else {
+		for _, v := range s.columns {
+			sql.WriteString(fmt.Sprintf("%s, ", v))
+		}
+	}
+
+	err = s.Next.Walk(sql)
+	return
+}
+
+// FromNode from节点
+type FromNode struct {
+	BaseNode
+	table string
+}
+
+func (f *FromNode) Walk(sql *strings.Builder) (err error) {
+	_, err = sql.WriteString(fmt.Sprintf("FROM %s ", f.table))
+	if err != nil {
+		return
+	}
+
+	err = f.Next.Walk(sql)
+	return
+}
+
+// WhereNode where节点
+type WhereNode struct {
+	BaseNode
+	cond string
+}
+
+func (w *WhereNode) Walk(sql *strings.Builder) (err error) {
+	_, err = sql.WriteString(fmt.Sprintf("WHERE %s ", w.cond))
+	if err != nil {
+		return
+	}
+
+	err = w.Next.Walk(sql)
+	return
 }
---
+++ stmt.go
@@ -0,0 +1,4 @@
+package torm
+
+type SelectStmt struct {
+}
